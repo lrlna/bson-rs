6281407f3badc5959232efcf8a7a4758f8ab204c
aherlihy <anna.herlihy@10gen.com>
2019-05-20 11:59:21 +0200

    with printlines

--- tests/modules/dec128_corpus.rs
+++ tests/modules/dec128_corpus.rs
@@ -1,18 +1,13 @@
 extern crate serde_derive;
-extern crate serde_json;
+//extern crate serde_json;
 use self::serde_derive::Deserialize;
-use self::serde_json::Value;
+//use self::serde_json::Value;
 
-use bson::oid::ObjectId;
-use bson::spec::BinarySubtype;
-use bson::{decode_document, decode_document_utf8_lossy, encode_document, Bson};
-use byteorder::{LittleEndian, WriteBytesExt};
+use bson::{decode_document, Bson};
 use bson::decimal128::Decimal128;
-use chrono::offset::TimeZone;
-use chrono::Utc;
-use std::io::{Cursor, Write, BufReader};
+use std::io::{Cursor, BufReader};
 use std::env;
-use std::fs::{self, DirEntry, File};
+use std::fs::{self, File};
 use std::path::Path;
 
 
@@ -42,42 +37,59 @@ struct TestFile {
     valid: Vec<Test>
 }
 
+#[derive(Deserialize, Debug)]
+struct D128 {
+    #[serde(rename="$numberDecimal")]
+    decimal128: String
+}
+
+#[derive(Deserialize, Debug)]
+struct Decimal128ExtJSON {
+    d: D128
+}
+
+fn run_test(description: &str, canonical_bson: &str, canonical_extjson: &str) {
+    println!("\tDESCRIPTION {}", description);
+
+    // Extract string value from extended JSON
+    let d128: Decimal128ExtJSON = serde_json::from_str(canonical_extjson).unwrap();
+    let extjson_str = d128.d.decimal128;
+    println!("\t\tEXPECTED STRING {}", extjson_str);
+
+    // Decode and build document
+    let bson_bytes = hex::decode(canonical_bson).unwrap();
+    let decoded = decode_document(&mut Cursor::new(bson_bytes)).unwrap();
+
+    let decoded_str = decoded.get("d").unwrap();
+    match decoded_str {
+        Bson::Decimal128(val) => {
+            println!("\t\tACTUAL STRING: {}", val.to_string());
+//            assert_eq!(extjson_str, val.to_string());
+        }
+        _ => unimplemented!(),
+    }
+}
+
 #[test]
 fn test_encode_decode_decimal128_corpus() { // TODO: get rid of all the unwraps()
 
     // These three values are eventually going to come from the test JSON
     let canonical_bson = "180000001364000000000000000000000000000000007C00";
     let canonical_extjson = "{\"d\" : {\"$numberDecimal\" : \"NaN\"}}";
-    let extjson_str = "NaN";
 
     let path = env::current_dir().unwrap().join(Path::new("tests/modules/corpus")); // TODO: need to join everything?
 
     let test_files = fs::read_dir(path).unwrap();
     for fp in test_files {
         let file_path = fp.unwrap().path();
-        println!("{}", file_path.display());
+        println!("FILE: {}", file_path.display());
         let file = File::open(file_path).unwrap();
         let reader = BufReader::new(file);
         let tests: TestFile = serde_json::from_reader(reader).unwrap();
         for test in tests.valid {
-            println!("{}", test.description);
+            run_test(&test.description, &test.canonical_bson, &test.canonical_extjson);
         }
 
-
-    }
-
-    // Decode and build document
-    let bson_bytes = hex::decode(canonical_bson).unwrap();
-    let decoded = decode_document(&mut Cursor::new(bson_bytes)).unwrap();
-
-    println!("{}", decoded.to_string());
-    let decoded_str = decoded.get("d").unwrap(); // TODO: decoded_str.as_string(), i.e. find this method!
-    match decoded_str {
-        Bson::Decimal128(val) => {
-            println!("{}", val.to_string());
-            assert_eq!(extjson_str, val.to_string());
-        }
-        _ => unimplemented!(),
     }
 
 }
